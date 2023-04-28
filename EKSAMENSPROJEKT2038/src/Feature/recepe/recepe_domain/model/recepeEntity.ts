99f53e87ce11606fdc8e0da036d98d343aa2f794
Pernlle <pkoe37782@edu.ucl.dk>
2023-04-28 13:36:49 +0200

    recepeEntity

--- EKSAMENSPROJEKT2038/src/Feature/recepe/recepe_domain/model/recepeEntity.ts
+++ EKSAMENSPROJEKT2038/src/Feature/recepe/recepe_domain/model/recepeEntity.ts
@@ -1,3 +1,26 @@
-export class recepe{
-    
+export class recepe {
+  id;
+  name;
+  description;
+  ingredients;
+  instructions;
+  nutrisionalInformation?;
+  category;
+  constructor(
+    id: number,
+    name: string,
+    description: string,
+    ingredients: string,
+    instructions: string,
+    nutritionalInformation: string,
+    category: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.nutrisionalInformation = nutritionalInformation;
+    this.category = category;
+  }
 }
